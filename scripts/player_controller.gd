cd3259248f533c99139f7cb8d8c6cc11d3c47418
Teodor Angeleski <teodorangeleski@gmail.com>
2025-01-25 11:09:56 +0100

    add mouse camera movement and player rotation

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,23 +1,33 @@
 extends CharacterBody3D
 
+@onready var camera_spring: SpringArm3D = $CamRoot/SpringArm3D
 
+const MOUSE_SENSITIVITY = 0.005
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			self.rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+			camera_spring.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+			camera_spring.rotation.x = clamp(camera_spring.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta):
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
